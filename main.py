dc014f38376f5d99896e0bfb155b716495a4b895
muhammadwarrad <moe_warrad@yahoo.com>
2023-02-05 13:16:57 -0500

    Connect to the database and create the table

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ class Note(Model):
     class Meta:
         database = db
         
+def create_tables():
+    db.connect()
+    db.create_tables([Note], safe=True)
+
+def seed_data():
+    note1 = Note(title='Note 1', content='first note blah.')
+    note2 = Note(title='Note 2', content='fire noteee')
+    note1.save()
+    note2.save()
